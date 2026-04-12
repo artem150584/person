9bd9b317b088e6b423e03fa1057b91decd60913d
User <email@someemail.com>
2026-04-12 16:49:23 +0500

    Task 10: modified dependency module modified url for external service

--- person-service-impl/src/main/java/com/study/client/AuthClient.java
+++ person-service-impl/src/main/java/com/study/client/AuthClient.java
@@ -4,7 +4,7 @@ package com.study.client;
 import com.study.controller.AuthController;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "auth-controller-client", url = "http://localhost:8081/api/v1/auth")
+@FeignClient(name = "auth-controller-client", url = "http://localhost:8081")
 public interface AuthClient extends AuthController {
 
 }
